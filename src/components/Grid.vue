1668793e3c3c29503dbfb98005826a1a0fe85f16
LynnKomang <lynnkomang98@gmail.com>
2020-10-09 12:27:47 +0300

    Finished basic grid logic

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,3 +1,52 @@
 <template>
-  <div></div>
+  <div class="grid" :style="gridStyle">
+    <div
+      v-for="cellNum in size * size"
+      :key="cellNum"
+      @mouseover="highlight($event)"
+      @mouseout="unhighlight($event)"
+    ></div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      size: 5,
+    };
+  },
+  computed: {
+    gridStyle() {
+      return {
+        gridTemplateColumns: `repeat(${this.size}, minmax(100px, 1fr))`,
+      };
+    },
+  },
+  methods: {
+    highlight(event) {
+      event.target.style.backgroundColor = "red";
+    },
+    unhighlight(event) {
+      event.target.style.backgroundColor = "white";
+    },
+  },
+};
+</script>
+
+<style>
+.grid {
+  display: grid;
+}
+
+.grid > div {
+  border: 1px solid black;
+}
+
+.grid > div::before {
+  content: "";
+  padding-bottom: 100%;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
