a535b4b6a3c4b2a93152b4ce5d2ccecc1f1ea217
LynnKomang <lynnkomang98@gmail.com>
2020-10-10 20:23:57 +0300

    Finished writing the A* pathfinding algorithm

---
+++ src/astar.js
@@ -0,0 +1,137 @@
+const MAX_F = Number.MAX_SAFE_INTEGER;
+
+const initiateMap = (height, width) => {
+  const map = Array.from({ length: height }, () =>
+    Array.from({ length: width }, () => ({
+      f: MAX_F,
+      g: MAX_F,
+      h: MAX_F,
+      parent: null,
+    }))
+  );
+
+  for (let row = 0; row < height; ++row) {
+    for (let col = 0; col < width; ++col) {
+      map[row][col].pos = { row, col };
+    }
+  }
+
+  return map;
+};
+
+const isValid = (map, row, col) => {
+  return row >= 0 && row < map.length && col >= 0 && col < map[0].length;
+};
+
+const calcDistance = (q, successor) => {
+  return q.pos.row === successor.pos.row || q.pos.col === successor.pos.col ? 1 : Math.SQRT2;
+};
+
+const calcH = (successor, goal) => {
+  return Math.sqrt(
+    (successor.pos.col - goal.pos.col) ** 2 + (successor.pos.row - goal.pos.row) ** 2
+  );
+};
+
+const tracePath = (map, goal) => {
+  const path = Array.from({ length: map.length }, () =>
+    Array.from({ length: map[0].length }, () => 0)
+  );
+
+  while (goal.parent != goal) {
+    const { row, col } = goal.pos;
+
+    path[row][col] = 1;
+
+    goal = goal.parent;
+  }
+
+  const { row, col } = goal.pos;
+  path[row][col] = 1;
+
+  return path;
+};
+
+const checkSuccessor = (map, goal, open, closed, q, rowAdd, colAdd) => {
+  let wasFound = false;
+  const coords = q.pos;
+
+  if (isValid(map, coords.row + rowAdd, coords.col + colAdd)) {
+    // A valid neighbor was found
+    const successor = map[coords.row + rowAdd][coords.col + colAdd];
+
+    if (successor === goal) {
+      successor.parent = q;
+      console.log('GOAL FOUND!');
+      wasFound = true;
+    } else if (!closed.has(successor)) {
+      const newG = q.g + calcDistance(q, successor);
+      const newH = calcH(successor, goal);
+      const newF = newG + newH;
+
+      if (successor.f === MAX_F || successor.f > newF) {
+        open.add(successor);
+        successor.f = newF;
+        successor.h = newH;
+        successor.g = newG;
+        successor.parent = q;
+      }
+    }
+  }
+
+  return wasFound;
+};
+
+const findPath = (map, start, goal) => {
+  const open = new Set();
+  const closed = new Set();
+
+  start.parent = start;
+  start.f = 0;
+  start.g = 0;
+  start.h = 0;
+
+  open.add(start);
+
+  let wasFound = false;
+
+  while (open.size > 0 && !wasFound) {
+    const q = open.values().next().value;
+    open.delete(q);
+    closed.add(q);
+
+    // Generate all the neighbors of the current node
+    for (let row = -1; row <= 1 && !wasFound; ++row) {
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, 1, 0);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, -1, 0);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, 0, 1);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, 0, -1);
+
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, 1, 1);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, -1, -1);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, -1, 1);
+      wasFound = wasFound || checkSuccessor(map, goal, open, closed, q, 1, -1);
+    }
+  }
+
+  if (!wasFound) {
+    console.log('Unable to find path...');
+
+    return [];
+  }
+
+  return tracePath(map, goal);
+};
+
+const width = 10;
+const height = 10;
+
+const map = initiateMap(height, width);
+
+console.table(
+  findPath(
+    map,
+    map[Math.floor(Math.random() * map.length)][Math.floor(Math.random() * map[0].length)],
+    map[Math.floor(Math.random() * map.length)][Math.floor(Math.random() * map[0].length)]
+  )
+);
